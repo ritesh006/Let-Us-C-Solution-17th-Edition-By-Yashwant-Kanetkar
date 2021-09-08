92af92c068f7c6db743df61d747e03f55a595e31
ritesh006 <ritesh.kumar0793@gmail.com>
2021-09-08 20:32:00 +0530

    this file delete after project complete

---
+++ test.c
@@ -0,0 +1,31 @@
+/* If ages of Ram, Shaym and Ajay are input through the keyboard write a program to determine the 
+youngest of the three*/
+
+#include<stdio.h>
+int main()
+{
+    int r,s,a;
+    int st1;
+    printf("Please enter Ram, Shyam and Ajay age: ");
+    scanf("%d %d %d",&r,&s,&a);
+
+    if(r<s)
+    {
+        if(r<a)
+        {
+           printf("Ram is YOnger");     
+        }
+    }
+    
+if (s<r)
+    {
+        if(s<a)
+        printf("Shayam is younger");
+        else 
+    {
+        printf("Ajay is younger");
+    }
+    }
+    
+       
+}   
